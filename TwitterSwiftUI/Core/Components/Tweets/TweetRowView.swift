6aea6684e422f62da088d07c7ba1eca4d3e0589d
Cliftonia <clifton.baggerman@me.com>
2022-06-13 10:48:08 +1000

    first commit

---
+++ TwitterSwiftUI/Core/Components/Tweets/TweetRowView.swift
@@ -0,0 +1,20 @@
+//
+//  TweetRowView.swift
+//  TwitterSwiftUI
+//
+//  Created by Clifton Theodore on 13/6/2022.
+//
+
+import SwiftUI
+
+struct TweetRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TweetRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        TweetRowView()
+    }
+}
