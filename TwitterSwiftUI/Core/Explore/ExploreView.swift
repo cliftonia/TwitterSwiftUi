7d38e62ec46eb4148101b5d5927d5c8ef7cf3b44
Cliftonia <clifton.baggerman@me.com>
2022-06-13 11:00:51 +1000

    Built MainTab and Feed View Style

--- TwitterSwiftUI/Core/Explore/ExploreView.swift
+++ TwitterSwiftUI/Core/Explore/ExploreView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct ExploreView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        Text("ExploreView")
+    } 
 }
 
 struct ExploreView_Previews: PreviewProvider {
