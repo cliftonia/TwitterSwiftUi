ef65ff6dde911855d46381ec48491c7cb1f5239b
Cliftonia <clifton.baggerman@me.com>
2022-06-17 15:54:21 +1000

    Created UserRowView, ExploreView and updated Profile View with presentation mode

---
+++ TwitterSwiftUI/Core/Components/Users/UserRowView.swift
@@ -0,0 +1,37 @@
+//
+//  UserRowView.swift
+//  TwitterSwiftUI
+//
+//  Created by Clifton Theodore on 17/6/2022.
+//
+
+import SwiftUI
+
+struct UserRowView: View {
+    var body: some View {
+        HStack(spacing: 12) {
+            Circle()
+                .frame(width: 48, height: 48)
+            
+            VStack(alignment: .leading, spacing: 4) {
+                Text("Joker")
+                    .font(.subheadline).bold()
+                    .foregroundColor(.black)
+                
+                Text("Heath Ledger")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }
+            
+            Spacer()
+        }
+        .padding(.horizontal)
+        .padding(.vertical, 4)
+    }
+}
+
+struct UserRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserRowView()
+    }
+}
