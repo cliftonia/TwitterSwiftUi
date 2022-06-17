46413fb129ba4b4c36fc618b00348bbfcfb42a8c
Cliftonia <clifton.baggerman@me.com>
2022-06-17 16:16:20 +1000

    UserStatsView, Updated ProfileView,  Updated SideMenuViewModel, Updated SideMenuView

---
+++ TwitterSwiftUI/Core/Components/Users/UserStatsView.swift
@@ -0,0 +1,20 @@
+//
+//  UserStatsView.swift
+//  TwitterSwiftUI
+//
+//  Created by Clifton Theodore on 17/6/2022.
+//
+
+import SwiftUI
+
+struct UserStatsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserStatsView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserStatsView()
+    }
+}
---
+++ TwitterSwiftUI/Core/SideMenu/ViewModels/SideMenuViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SideMenuViewModel.swift
+//  TwitterSwiftUI
+//
+//  Created by Clifton Theodore on 17/6/2022.
+//
+
+import Foundation
