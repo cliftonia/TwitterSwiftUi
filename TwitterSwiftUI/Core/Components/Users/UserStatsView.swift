9518e322ab62f70516e134687f807f652abb3e9d
Cliftonia <clifton.baggerman@me.com>
2022-06-17 16:16:28 +1000

    UserStatsView, Updated ProfileView,  Updated SideMenuViewModel, Updated SideMenuView

--- TwitterSwiftUI/Core/Components/Users/UserStatsView.swift
+++ TwitterSwiftUI/Core/Components/Users/UserStatsView.swift
@@ -9,7 +9,27 @@ import SwiftUI
 
 struct UserStatsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 24) {
+            HStack(spacing: 4) {
+                Text("807")
+                    .font(.subheadline)
+                    .bold()
+                
+                Text("Following")
+                    .font(.caption)
+                    .foregroundColor(.gray)
+            }
+            
+            HStack(spacing: 4) {
+                Text("6.9M")
+                    .font(.subheadline)
+                    .bold()
+                
+                Text("Followers")
+                    .font(.caption)
+                    .foregroundColor(.gray)
+            }
+        } 
     }
 }
 
--- TwitterSwiftUI/Core/SideMenu/Views/SideMenuView.swift
+++ TwitterSwiftUI/Core/SideMenu/Views/SideMenuView.swift
@@ -9,7 +9,44 @@ import SwiftUI
 
 struct SideMenuView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(alignment: .leading, spacing: 32) {
+            VStack(alignment: .leading) {
+                Circle()
+                    .frame(width: 48, height: 48)
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("Bruce Wayne")
+                        .font(.headline)
+                    
+                    Text("@batman")
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                }
+                
+                UserStatsView()
+                    .padding(.vertical)
+                
+            }
+            .padding(.leading)
+            
+            ForEach(SideMenuViewModel.allCases, id: \.rawValue) { option in
+                HStack(spacing: 16) {
+                    Image(systemName: option.imageName)
+                        .font(.headline)
+                        .foregroundColor(.gray)
+                    
+                    Text(option.title)
+                        .font(.subheadline)
+                    
+                    Spacer()
+                }
+                .frame(height: 40)
+                .padding(.horizontal)
+            }
+            
+            Spacer()
+        }
     }
 }
 
